9ac7b4239e533bd381d64fcee75da20441805db1
saurabh <saurabharadwaj@gmail.com>
2018-11-06 16:10:03 +0530

    new edit of HelloWTest in master

--- HelloWrldTest/src/HelloWTest.java
+++ HelloWrldTest/src/HelloWTest.java
@@ -5,7 +5,7 @@ public class HelloWTest {
 		// TODO Auto-generated method stub
 		System.out.println("hello to world via git and eclipse - author - saurabh 11/04/2018 @ 12:11pm (UTC)");
 		System.out.println("tell saurabh that this project was edited by ashutosh");
-		System.out.println("new edit of HelloWTest on 11/06/2018 @ 7:26am (UTC) into dev_saur branch ");
+		System.out.println("new edit of HelloWTest on 11/06/2018 @ 7:26am (UTC) into master branch ");
 	}
 
 }
